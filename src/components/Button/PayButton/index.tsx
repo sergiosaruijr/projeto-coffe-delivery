73fed3e3d7b63ac3c472e3094116d05cc59d89e0
Sergio Akira Sarui Junior <sergiosaruijr@gmail.com>
2024-04-24 01:38:34 -0300

    arrumando detalhes do button

--- src/components/Button/PayButton/index.tsx
+++ src/components/Button/PayButton/index.tsx
@@ -4,7 +4,6 @@ import { ButtonContainer } from './styles'
 interface PayButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   text: string | number
 }
-
 export function PayButton({ text, ...rest }: PayButtonProps) {
   return <ButtonContainer {...rest}>{text}</ButtonContainer>
 }
